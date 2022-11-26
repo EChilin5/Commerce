b8eb6eaf47cf65c76c39233b73e28e71a19aec60
EChilin5 <edgar3ac@gmail.com>
2022-11-25 22:34:25 -0800

    product detail update

--- src/Components/InventoryComponents/InventoryProductDetails.js
+++ src/Components/InventoryComponents/InventoryProductDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Button from "react-bootstrap/Button";
 import "./IventoryProductDetails.css";
 
 export const InventoryProductDetails = () => {
@@ -52,23 +53,48 @@ export const InventoryProductDetails = () => {
 
   return (
     <div className="inventory-analysis">
-      <h1>Item Analysis section</h1>
-      <div>
-        <div>Rank 1/20</div>
-        <div>{productDetails[productDetails.length - 1].productName}</div>
-        <div>${productDetails[productDetails.length - 1].productPrice}</div>
+      <div className="inventory-analysis-header">
+        <h1>Item Analysis section</h1>
+        <Button>Update Items</Button>
+      </div>
 
-        <div>
-          {" "}
-          Item Id #{productDetails[productDetails.length - 1].productsId}
+      <div>
+        <div className="product-details-one">
+          <div className="product-rank">Rank 1/20</div>
+          <div className="product-details-one-mid">
+            <div>{productDetails[productDetails.length - 1].productName}</div>
+            <div>${productDetails[productDetails.length - 1].productPrice}</div>
+          </div>
+          <div>
+            <img
+              src={productDetails[productDetails.length - 1].productImage}
+              alt=""
+              width="300px"
+              height="200px"
+            />
+          </div>
         </div>
-        <div>
-          <img
-            src={productDetails[productDetails.length - 1].productImage}
-            alt=""
-            width="300px"
-            height="200px"
-          />
+        <div className="product-details-description">
+          {" "}
+          <h4>Product Description</h4>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </div>
+
+        <div className="product-details-bar-charts">
+          <div className="chart-left">
+            <h4>Sale Rate</h4>
+          </div>
+          <div className="chart-right">
+            <h4>Return Rate</h4>
+          </div>
         </div>
       </div>
     </div>
